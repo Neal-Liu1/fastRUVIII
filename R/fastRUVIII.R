3aea03cac6c1df8174db7ea857d5bbca8eb66094
Neal Liu <neal.liu18@gmail.com>
2025-03-24 21:03:45 +1100

    rename file

--- R/fastRUVIII.R
+++ R/fastRUVIII.R
@@ -1,14 +1,29 @@
-#' Run RUVIII for a data matrix
-#' @description Runs RUVIII and outputs an adjusted matrix
-#'
+#' Run fastRUVIII
+#' @description Runs fastRUVIII
 #' @param matrix A matrix with rows as features/genes and columns as cells.
 #' @param prpc A matrix of your pseudo replicates, obtained using the CreatePRPC function.
 #' @param ncgs A logical indicating which genes are control genes
 #' @param k How much unwanted variation to remove. Lower k = remove less, high k = remove more.
 #' @return An adjusted (log transformed) matrix with cells as columns and features as rows.
 #' @export
+#'
+#'
+#'
+setGeneric(
+  name = 'fastRUVIII',
+  function(
+    object,
+    k,
+    replicates = NULL,
+    control_genes = NULL,
+    assay = NULL,
+    apply_log = T)
+  {standardGeneric('fastRUVIII')})
+
+
+
 fastRUVIII_Matrix <- function(matrix, prpc, ncgs, k){
-  matrix <- as(matrix, 'CSparseMatrix')
+  matrix <- as(matrix, 'dgCMatrix')
   results <- Sparse_RUV_III(
     log2_sparse(t(matrix)),
     Yrep = t(prpc),
